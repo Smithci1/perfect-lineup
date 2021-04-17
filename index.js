8d451bc35cc1c85be974efa90163174279a50a02
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-17 13:07:51 -0400

    destructured lineup, created a function to set the salary cap, hasnt been proven to work just yet.

---
+++ index.js
@@ -0,0 +1,18 @@
+//destructure lineup
+const [{id},{name},{position},{gameId},{teamId},{salary}] = lineup
+//create a function that puts on the salary cap.
+const salaryCap = (lineup) => {
+    for (let i = 0; i < lineup.length;){
+        if(salary[i] > 45,000){
+            return false
+        }
+    }
+    
+ 
+
+//create validatelineup function
+const validateLindeup = () => {
+    
+}
+
+module.exports = validateLindeup
