f31ef77d4f3590e588b93fde76cf97aa78055805
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-18 05:34:32 -0400

    started to work on the max team

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const validateLineup = (lineup) => {
              if (playerSalaries > (45,000))
             return false
    }
+   //maximum teammates and players from the same game
+   const games = [123, 115, 101, 134, 126, 131, 119, 123, 124]
+   const teams = [12, 11, 22, 20, 14, 18, 27, 15,]
+       const maxTracker = 0
+       for (let t = 0; t < lineup.length; t++){
+           if (lineup[t].teamId === teams)
+           maxTracker++
+       }
+      
 
 
 module.exports = validateLineup
